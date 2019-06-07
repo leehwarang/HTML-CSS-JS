29e2c55b260bdc496bf844f8f36035104ba4a1b6
leehwarang <hwrng2@gmail.com>
2019-06-07 14:50:31 +0900

    feat 입력한 돌에서 대각선 4방향에 있는 상대편의 돌 뒤집는 기능 추가(리팩토링 전)

--- othello/game.js
+++ othello/game.js
@@ -13,11 +13,11 @@ class Game {
       [-1, 0], // 상
       [1, 0], // 하
       [0, -1], // 좌
-      [0, 1] // 우
-      // [-1, 1],
-      // [1, 1],
-      // [1, -1],
-      // [-1, -1]
+      [0, 1], // 우
+      [-1, 1], //오른쪽 대각선 위
+      [1, 1], // 오른쪽 대각선 아래
+      [-1, -1], // 왼쪽 대각선 위
+      [1, -1] // 왼쪽 대각선 아래
     ];
   }
 
@@ -48,12 +48,15 @@ class Game {
     return this.board[row][column] === 1 || this.board[row][column] === 0;
   }
 
+  // reverseStone(startRow, changeRowVal, startColumn, changeColumnVal, stack) {
+  // }
+
   searchUniqueDirection(coordinate, index, startRow, startColumn) {
     let changeRowVal = coordinate[0];
     let changeColumnVal = coordinate[1];
 
     let stack = [[startRow, startColumn]];
-    let target, x, y;
+    let x, y, target;
 
     switch (index) {
       case 0: // 상
@@ -61,6 +64,7 @@ class Game {
           x = startRow + changeRowVal;
           y = startColumn + changeColumnVal;
           target = this.board[x][y];
+
           if (target === this.currentStoneColor) {
             stack.forEach(val => {
               this.board[val[0]][val[1]] = this.currentStoneColor;
@@ -141,6 +145,118 @@ class Game {
             stack.push([x, y]);
           }
           startColumn += 1;
+        }
+        break;
+
+      case 4: // 오른쪽 대각선 위 [-1, 1]
+        console.log("오른쪽 대각선 위를 처리를 하나요?");
+        while (startRow !== 0 || startColumn !== 7) {
+          x = startRow + changeRowVal;
+          y = startColumn + changeColumnVal;
+          target = this.board[x][y];
+          console.log(`x좌표 : ${x}, y좌표 : ${y} / target : ${target}`);
+
+          if (target === this.currentStoneColor) {
+            stack.forEach(val => {
+              this.board[val[0]][val[1]] = this.currentStoneColor;
+            });
+
+            stack = [];
+            break;
+          } else if (target === 7) {
+            stack = [];
+
+            break;
+          } else {
+            stack.push([x, y]);
+            console.log(stack);
+          }
+          startRow -= 1;
+          startColumn += 1;
+        }
+        break;
+
+      case 5: // 오른쪽 대각선 아래 [1, 1]
+        console.log("오른쪽 대각선 아래를 처리를 하나요?");
+        while (startRow !== 7 || startColumn !== 7) {
+          x = startRow + changeRowVal;
+          y = startColumn + changeColumnVal;
+          target = this.board[x][y];
+          console.log(`x좌표 : ${x}, y좌표 : ${y} / target : ${target}`);
+
+          if (target === this.currentStoneColor) {
+            stack.forEach(val => {
+              this.board[val[0]][val[1]] = this.currentStoneColor;
+            });
+
+            stack = [];
+            break;
+          } else if (target === 7) {
+            stack = [];
+
+            break;
+          } else {
+            stack.push([x, y]);
+            console.log(stack);
+          }
+          startRow += 1;
+          startColumn += 1;
+        }
+        break;
+
+      case 6: // 왼쪽 대각선 위 [-1, -1]
+        console.log("왼쪽 대각선 위를 처리 하나요?");
+        while (startRow !== 0 || startColumn !== 0) {
+          x = startRow + changeRowVal;
+          y = startColumn + changeColumnVal;
+          target = this.board[x][y];
+          console.log(`x좌표 : ${x}, y좌표 : ${y} / target : ${target}`);
+
+          if (target === this.currentStoneColor) {
+            stack.forEach(val => {
+              this.board[val[0]][val[1]] = this.currentStoneColor;
+            });
+
+            stack = [];
+            break;
+          } else if (target === 7) {
+            stack = [];
+
+            break;
+          } else {
+            stack.push([x, y]);
+            console.log(stack);
+          }
+          startRow -= 1;
+          startColumn -= 1;
+        }
+        break;
+
+      case 7: // 왼쪽 대각선 아래 [1, -1]
+        console.log("왼쪽 대각선 아래를 처리 하나요?");
+        while (startRow !== 7 || startColumn !== 0) {
+          x = startRow + changeRowVal;
+          y = startColumn + changeColumnVal;
+          target = this.board[x][y];
+          console.log(`x좌표 : ${x}, y좌표 : ${y} / target : ${target}`);
+
+          if (target === this.currentStoneColor) {
+            stack.forEach(val => {
+              this.board[val[0]][val[1]] = this.currentStoneColor;
+            });
+
+            stack = [];
+            break;
+          } else if (target === 7) {
+            stack = [];
+
+            break;
+          } else {
+            stack.push([x, y]);
+            console.log(stack);
+          }
+          startRow += 1;
+          startColumn -= 1;
         }
         break;
 
