3a54f11622b2bbd49e4daac670676f9b3a86aed1
leehwarang <hwrng2@gmail.com>
2019-06-18 00:27:51 +0900

    refactor 캐러셀을 클래스로 구조 변경

--- instagram/script.js
+++ instagram/script.js
@@ -1,10 +1,10 @@
 var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".trigger");
+var viewmoreImg = document.querySelector(".view-more-img");
 var closeButton = document.querySelector(".btn-close");
 
 var header_wrap = document.querySelector(".header-wrap");
 var header = document.querySelector(".header");
-var second_logo = document.querySelector(".second-logo");
+var textLogo = document.querySelector(".logo-text");
 
 function toggleModal() {
   modal.classList.toggle("show-modal");
@@ -24,15 +24,48 @@ function Scroll() {
   if (ypos > 50) {
     header_wrap.classList.add("short-header-wrap");
     header.classList.add("short-header");
-    second_logo.classList.add("hide-second-logo");
+    textLogo.classList.add("hide-second-logo");
   } else {
     header_wrap.classList.remove("short-header-wrap");
     header.classList.remove("short-header");
-    second_logo.classList.remove("hide-second-logo");
+    textLogo.classList.remove("hide-second-logo");
   }
 }
 
-trigger.addEventListener("click", toggleModal);
+viewmoreImg.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 window.addEventListener("scroll", Scroll);
+
+class Carousel {
+  constructor(targetTag) {
+    this.carousel = document.querySelector(`.${targetTag}`);
+    this.carouselImg = this.carousel.querySelector(".upload-img");
+    this.prevBtn = document.querySelector(".prev");
+    this.nextBtn = document.querySelector(".next");
+    this.currentPointer = this.carousel.offsetLeft;
+  }
+
+  setCarouselLeftPosition(direction) {
+    this.currentPointer += direction * this.carouselImg.offsetWidth;
+    this.carousel.style.left = `${this.currentPointer}px`;
+  }
+
+  attachEventToBtn(btnTag) {
+    btnTag.addEventListener("click", () => {
+      if (btnTag.classList.contains("prev")) {
+        this.setCarouselLeftPosition(1);
+      } else if (btnTag.classList.contains("next")) {
+        this.setCarouselLeftPosition(-1);
+      }
+    });
+  }
+
+  init() {
+    this.attachEventToBtn(this.prevBtn);
+    this.attachEventToBtn(this.nextBtn);
+  }
+}
+
+const mycarousel = new Carousel("carousel");
+mycarousel.init();
